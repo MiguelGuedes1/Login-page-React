757673cd76f6fe2b71532b7389e83e1257ebfe19
MiguelGuedes1 <miguelsilvaguedes1@gmail.com>
2023-06-04 00:09:55 +0100

    inputs ja com useSates

--- loginpage/src/pages/LoginPage/index.jsx
+++ loginpage/src/pages/LoginPage/index.jsx
@@ -1,11 +1,39 @@
 import react from "react"
+import "./styles.css"
+import { useState } from "react"
 
 const LoginPage = () => {
 
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log("Formulario submetido", { email }, { password })
+    }
+
+
+
     return (
-        <h1>
-            Login Page
-        </h1>
+        <div id="login">
+            <h1>Login do Sistema</h1>
+
+            <form className="form" onSubmit={handleSubmit}>
+                <div className="field">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                </div>
+
+                <div className="field">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                </div>
+
+                <div className="actions">
+                    <button type="submit">Entrar</button>
+                </div>
+            </form>
+        </div>
     )
 }
 
